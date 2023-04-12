722f47ba288e1d5ab889e2189b6a4141d2b8916c
Thorvald <91074943+Soralienne@users.noreply.github.com>
2023-04-12 21:22:08 +0200

    animation mouvement

--- Tales_Of_BervinUnity/Assets/Scripts/PlayerMovement.cs
+++ Tales_Of_BervinUnity/Assets/Scripts/PlayerMovement.cs
@@ -13,6 +13,7 @@ public class PlayerMovement : MonoBehaviour
 
     public Rigidbody2D rb;
     public Animator animator;
+    public SpriteRenderer spriteRenderer;
     private Vector3 velocity = Vector3.zero;
 
     void FixedUpdate()
@@ -28,6 +29,8 @@ public class PlayerMovement : MonoBehaviour
 
         MovePlayer(horizontalMovement);
 
+        Flip(rb.velocity.x);
+
         float characterVelocity = Mathf.Abs(rb.velocity.x);
         animator.SetFloat("Speed", characterVelocity);
     }
@@ -41,6 +44,17 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.AddForce(new Vector2(0f, jumpForce));
             isJumping = false;
+        }
+    }
+
+    void Flip(float _velocity)
+    {
+        if (_velocity > 0.1f)
+        {
+            spriteRenderer.flipX = false;
+        } else if (_velocity < -0.1f)
+        {
+            spriteRenderer.flipX = true;
         }
     }
 }
